42cb5ef08fd3bf96575278985980b80fd64857a2
Harmanpreet Singh <harmanpreet.singh@yale.edu>
2016-01-18 19:35:13 -0500

    _

--- views/login_form.php
+++ views/login_form.php
@@ -13,6 +13,15 @@
         </div>
     </fieldset>
 </form>
-<div>
-    or <a href="register.php">register</a> for an account
+<div class="non-login">
+    <a href="register.php">register for an account</a>
+</div>
+
+<div class="non-login">
+    <a href="https://youtu.be/zCMMFlwBx_o" target="_blank">watch an explanatory video</a>
+</div>
+
+<div class="non-login">
+    <a href="http://www.youtube.com/watch?v=TWw28ZwjQvE&t=73m31s" target="_blank">
+        watch the founders explain the backend logic</a>
 </div>
